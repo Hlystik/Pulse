6e71ad1380389b20d47b7319891258da17df01dc
Anatoly <hlystike@rambler.ru>
2022-04-29 08:03:28 +0700

    add carousel dots

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,23 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 991,
                 settings: {
+                    infinite: true,
                     dots: true,
-                    arrow: false
+                    arrows: false
                 }
             }
         ]
     });
 });
+/* var slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false
+}); */
